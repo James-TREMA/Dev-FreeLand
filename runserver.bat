28499d7d4ca38bf42d3e5a0981fa6211f5d60df1
James <43522217+James-TREMA@users.noreply.github.com>
2020-08-26 04:14:22 +0200

    Ajout .bat "runserver" pour restart

---
+++ runserver.bat
@@ -0,0 +1,14 @@
+@echo OFF
+RMDIR /s /q "C:\FXServer\cache\files"
+echo ----------------------------------
+echo -
+echo Pour relancer votre serveur, faites CTRL + C puis "runserver"
+echo -
+echo ----------------------------------
+echo -
+echo Appuyez sur une TOUCHE pour lancer votre serveur
+echo -
+pause > nul
+CLS
+cd cC:\FXServer
+cmd /k run.cmd +exec server.cfg
--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo OFF
-RMDIR /s /q "e:\FXServer\cache\files"
-
-echo Appuyez sur une touche pour démarrer
-pause > nul
-CLS
-cd e:\FXServer
-cmd /k run.cmd +exec server.cfg
